8ad4bcb7ff0f118a569c10eb1b71110f692a73ff
Ali Jouni <alijouni@outlook.de>
2021-09-08 20:25:52 +0300

    Adding ErrorBoundary

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Card =({name,email,id}) =>{ 
+
 	return(
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' >
 			<img alt='robots' src={`https://robohash.org/${id}?size=200x200&set=set4`}/>
